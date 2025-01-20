e0b6a1fbade7707183ac93e087d9d51343ac0a1f
joyeajin <dpwls31200@gmail.com>
2025-01-20 15:52:25 +0900

    drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "motion/react";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -11,6 +12,17 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* overflow: hidden; */
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -20,16 +32,27 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
+  hover: { rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
       <h1>framer motion</h1>
       <br />
-      <Box variants={boxVariants} whileHover="hover" whileTap="click"></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
